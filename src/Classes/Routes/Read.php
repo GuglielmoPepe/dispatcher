651f465c2fcee8f4a9c11693b8464c0030287e7d
Guglielmo Pepe <1424836+GuglielmoPepe@users.noreply.github.com>
2021-06-13 14:46:40 +0200

    Add Read route class

---
+++ src/Classes/Routes/Read.php
@@ -0,0 +1,46 @@
+<?php 
+
+declare(strict_types = 1);
+
+namespace Dispatcher\Classes\Routes;
+
+class Read implements \Dispatcher\Interfaces\Route
+{
+    public function connect() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+    public function delete() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+    public function get() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+    public function head() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+    public function options() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+    public function patch() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+    public function post() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+    public function put() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+    public function trace() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+}
+
