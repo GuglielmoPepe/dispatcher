233e517c89ef8b8eff02a0cdd2b07e440585a08a
Guglielmo Pepe <1424836+GuglielmoPepe@users.noreply.github.com>
2021-06-13 14:48:28 +0200

    Add Convention handler class

---
+++ src/Classes/Handlers/Convention.php
@@ -0,0 +1,47 @@
+<?php 
+
+declare(strict_types = 1);
+
+namespace Dispatcher\Classes\Handlers;
+
+class Convention implements \Dispatcher\Interfaces\Handler 
+{
+    private $successor;
+
+    public function __construct() 
+    {
+        $this->successor = NULL;
+    }
+
+    final public function connect(\Dispatcher\Interfaces\Handler $successor) : void 
+    {
+        if ($this->successor === NULL) 
+        {
+            $this->successor = $successor;
+        } 
+        else 
+        {
+            $this->successor->connect($successor);
+        }
+
+        return;
+    }
+
+    final public function handle(array $input) : \Dispatcher\Interfaces\Route
+    {
+        $route = '\Dispatcher\Classes\Routes\\' . isset($input['action']) ? ucfirst($input['action']) : 'Home';
+
+        if (class_exists($route)) 
+        {
+            return new $route();
+        }
+
+        if ($this->successor !== NULL) 
+        {
+            return $this->successor->handle($input);
+        }
+
+        throw new \Dispatcher\Classes\Exceptions\HandlerClassNotFound();
+    }
+}
+
