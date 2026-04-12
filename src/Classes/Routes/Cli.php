40b8bf72c277c69b58bfc2af2f2fdc8a021c697a
GuglielmoPepe <1424836+GuglielmoPepe@users.noreply.github.com>
2026-04-12 23:55:55 +0200

    Add Cli, Http and WebSocket routes

---
+++ src/Classes/Routes/Cli.php
@@ -0,0 +1,14 @@
+<?php 
+
+declare(strict_types = 1);
+
+namespace Dispatcher\Classes\Routes;
+
+class Cli implements \Dispatcher\Interfaces\Route
+{
+    public function execute() : void
+    {
+        throw new \Dispatcher\Classes\Exceptions\MethodCallNotAllowed();
+    }
+}
+
